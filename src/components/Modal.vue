078009b88539fc4f9702fa21b3bd71234c035ab3
Mike Dougllas <mdougllas@gmail.com>
2021-02-28 12:33:20 -0500

    book details added

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,8 +1,27 @@
 <template>
-    <b-modal id="modal-scrollable" size="xl" scrollable title="Book Title">
-        <b-card img-src="https://placekitten.com/300/300" img-alt="Card image" img-left class="mb-3">
+    <b-modal id="modal-scrollable" size="xl" scrollable :title="bookDetails ? bookDetails.title : 'Title not found'">
+        <b-card :img-src="`${bookDetails ? bookDetails.imageLinks.thumbnail : 'https://placekitten.com/300/300'}`" img-alt="Card image" img-left class="mb-3">
             <b-card-text>
-                {{ googleId }}
+                <b-row>
+                    <b-col v-if="bookDetails">
+                        <div>
+                            <h3>Authors:</h3>
+                            <ul v-for="(author, index) in bookDetails.authors" :key="index">
+                                <li>{{ author }}</li>
+                            </ul>
+                        </div>
+                    </b-col>
+                    <b-col v-if="bookDetails">
+                        <h3>Average Rating:</h3>
+                        {{ bookDetails.averageRating }}
+                    </b-col>
+                    <b-col v-if="bookDetails">
+                        <h3>Preview Book</h3>
+                            <b-button :href="bookDetails.previewLink" target="_blank" size="lg">
+                                See book preview
+                            </b-button>
+                    </b-col>
+                </b-row>
             </b-card-text>
         </b-card>
     </b-modal>
@@ -21,7 +40,7 @@ export default {
     },
 
     computed: {
-        ...mapState(['bookDetails'])
+        ...mapState({ bookDetails: state => state.bookSearch.bookDetails })
     }
 }
 </script>
