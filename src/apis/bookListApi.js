87f2bd355d4c1681aef496e4a6f6a8a4421c86a5
Mike Dougllas <mdougllas@gmail.com>
2021-02-28 15:33:39 -0500

    detail... man I'm tyred.

--- src/apis/bookListApi.js
+++ src/apis/bookListApi.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 export const bookListApi = axios.create(
     {
-        baseURL: 'http://localhost:8000/api',
-        // baseURL: 'https://booj-reading-list-api.herokuapp.com/api',
+        // baseURL: 'http://localhost:8000/api',
+        baseURL: 'https://booj-reading-list-api.herokuapp.com/api',
 
         headers: {
             'Content-Type': 'application/json'
